a394a1c8f2b38cbb182a929edab61f4f4f17bd48
daekyochung <chung5817@gachon.ac.kr>
2026-07-08 00:55:10 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/leets/tdd/TddBeApplication.java
@@ -0,0 +1,13 @@
+package com.leets.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddBeApplication.class, args);
+    }
+
+}
