4c621fdc78822d210842c99d130bad52be9a36a3
vyfhfhd <lucy9760@gachon.ac.kr>
2026-07-22 22:01:53 +0900

    fix: ê³비비번,닉네임, 기숙사 size 및 pattern 추가

--- src/main/java/com/leets/tdd/user/dto/ProfileRegistrationRequest.java
+++ src/main/java/com/leets/tdd/user/dto/ProfileRegistrationRequest.java
@@ -2,6 +2,7 @@ package com.leets.tdd.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 /**
  * 계정등록(회원가입 완료) 요청.
@@ -15,10 +16,14 @@ public record ProfileRegistrationRequest(
         String email,
 
         @NotBlank(message = "비밀번호를 입력해주세요.")
+        @Size(min = 8, max = 20, message = "비밀번호는 8~20자로 입력해주세요.")
         String password,
 
+        @Size(min = 2, max = 10, message = "닉네임은 2~10자로 입력해주세요.")
+        @Pattern(regexp = "^[가-힣a-zA-Z0-9]*$", message = "닉네임은 한글, 영문, 숫자만 사용할 수 있습니다.")
         String nickname,
 
+        @Pattern(regexp = "^(1기숙사|2기숙사|3기숙사)?$", message = "기숙사 동을 선택하세요(1기숙사).")
         String dormitory
 ) {
 }
