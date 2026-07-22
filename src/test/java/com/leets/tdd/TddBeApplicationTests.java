6920d6132f5f448ddf59e82c46caf8b84c595244
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-07-22 15:38:56 +0900

    ✅ test: 메일 발송기 테스트 빈 추가

--- src/test/java/com/leets/tdd/TddBeApplicationTests.java
+++ src/test/java/com/leets/tdd/TddBeApplicationTests.java
@@ -2,12 +2,27 @@ package com.leets.tdd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.mockito.Mockito.mock;
 
 @SpringBootTest
+@Import(TddBeApplicationTests.TestMailConfiguration.class)
 class TddBeApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
+    @TestConfiguration(proxyBeanMethods = false)
+    static class TestMailConfiguration {
+
+        @Bean
+        JavaMailSender javaMailSender() {
+            return mock(JavaMailSender.class);
+        }
+    }
 }
